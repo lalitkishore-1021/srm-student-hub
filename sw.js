ef567ef0440681b161f43540e90624f334e818a7
lalitkishore-1021 <blalitkishoregmail.com>
2026-08-24 00:18:27 +0530

    Bump cache version, add RESET button, and correctly move AI FAB inside home-view

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v18-speed-sync'; 
+const CACHE_NAME = 'srm-hub-v19-ui-update'; 
 
 const ASSETS_TO_CACHE = [
     '/',
