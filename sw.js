75df41a8973d171ea4614ccdd472c25169d68a88
lalitkishore-1021 <blalitkishoregmail.com>
2026-08-24 16:37:40 +0530

    Fix login card visibility on new devices

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v27-fix-blank'; 
+const CACHE_NAME = 'srm-hub-v28-fix-login'; 
 
 const ASSETS_TO_CACHE = [
     '/',
