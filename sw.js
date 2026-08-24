f4c747f329a22ca2d49f6b33d09ddeda0f59a924
lalitkishore-1021 <blalitkishoregmail.com>
2026-08-24 14:52:26 +0530

    Fix IndexedDB VersionError

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v23-logout-fix'; 
+const CACHE_NAME = 'srm-hub-v24-db-version'; 
 
 const ASSETS_TO_CACHE = [
     '/',
