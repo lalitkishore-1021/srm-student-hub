b301d182f164111d4daeee5a952e5a4ed5f3b98c
lalitkishore-1021 <blalitkishoregmail.com>
2026-08-24 17:19:14 +0530

    Fix UI: remove green background, fix transparent modal bg, fallback subjects in empty attendance

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v30-fix-scope'; 
+const CACHE_NAME = 'srm-hub-v31-fix-ui'; 
 
 const ASSETS_TO_CACHE = [
     '/',
