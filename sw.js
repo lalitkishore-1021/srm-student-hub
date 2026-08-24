9d20fb4ba95d88f715f9eee5899f6c12b47c8b59
lalitkishore-1021 <blalitkishoregmail.com>
2026-08-24 18:10:36 +0530

    Fix UI bugs: replace workingAttData.forEach, remove duplicate modals from template literal, fix text colors

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v31-fix-ui'; 
+const CACHE_NAME = 'srm-hub-v32-fix-modals'; 
 
 const ASSETS_TO_CACHE = [
     '/',
