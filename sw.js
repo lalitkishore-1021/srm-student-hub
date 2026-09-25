3150eb29d8b99e39bd9e877bb034a32d2265c592
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-26 00:57:12 +0530

    Fix duplicate updateVideoBackground call that cleared bgVideoPlayer.src on every track play

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v46-video-visibility-fix'; 
+const CACHE_NAME = 'srm-hub-v47-fix-duplicate-video-wipe'; 
 
 const ASSETS_TO_CACHE = [
     '/',
