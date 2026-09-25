3e1b8faf08dff161419409a46abaa06ad18198f1
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-26 00:01:49 +0530

    Fix music lounge video player to behave like Spotify Canvas: plays in background and fades out cover thumbnail only after buffering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v41-native-video-stream'; 
+const CACHE_NAME = 'srm-hub-v42-spotify-canvas-fix'; 
 
 const ASSETS_TO_CACHE = [
     '/',
