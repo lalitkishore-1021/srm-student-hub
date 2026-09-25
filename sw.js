37bcd603ace9072dcec53e34135805e9d1d7fbb8
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-25 08:23:17 +0530

    Fix music offline downloading: fetch and encode MP3 blob into Base64 instead of just saving the streaming URL to IndexedDB, enabling true offline playback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v34-music-popup'; 
+const CACHE_NAME = 'srm-hub-v35-offline-fix'; 
 
 const ASSETS_TO_CACHE = [
     '/',
