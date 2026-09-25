1e07fe206e6cba100aea82312a37cd09f60f56fb
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-25 08:33:57 +0530

    Fix music offline playback: fix strict type mismatches (String vs Number) that were preventing the app from finding the downloaded tracks in IndexedDB

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v35-offline-fix'; 
+const CACHE_NAME = 'srm-hub-v36-offline-types'; 
 
 const ASSETS_TO_CACHE = [
     '/',
