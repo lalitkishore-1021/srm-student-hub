2d575cbec8c458d7760241ebec078aad07d5e6a7
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-26 00:26:59 +0530

    Remove video from mini player to free up hardware decoder for fullscreen background video

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v43-lyrics-back-button'; 
+const CACHE_NAME = 'srm-hub-v44-video-decoder-fix'; 
 
 const ASSETS_TO_CACHE = [
     '/',
