870262446a096bb8fc170e81a23eeb37c143ad95
lalitkishore-1021 <blalitkishoregmail.com>
2026-09-26 00:34:03 +0530

    Fix background video preload issue and add robust playback detection events

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number every time you want to force phones to update!
-const CACHE_NAME = 'srm-hub-v44-video-decoder-fix'; 
+const CACHE_NAME = 'srm-hub-v45-bg-video-preload-fix'; 
 
 const ASSETS_TO_CACHE = [
     '/',
